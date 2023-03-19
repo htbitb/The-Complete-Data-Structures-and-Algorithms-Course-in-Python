9c882126f1588f5fa02fe3b0e3c50395d4efba57
htb <htb160521@gmail.com>
2023-03-19 15:24:45 +0700

    How to write Recursion in 3 steps

--- How to write Recursion in 3 steps.py
+++ How to write Recursion in 3 steps.py
@@ -18,6 +18,8 @@
 
 def factorial(n):
     assert n > 0 and int(n) == n, 'The number must be positive integer only!'
+    # this assert key word lets you test if the condition in your code return True, if not the
+    # program will raise an AssertionError than can be write by ownselves
     if n in [0, 1]:
         return 1
     else: 
